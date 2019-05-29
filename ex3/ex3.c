2b36db6ab265d8ed725f13bb136e708cdd162251
Matt Basile <matthewbasile2@gmail.com>
2019-05-29 16:53:21 -0400

    ex3

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -9,7 +9,15 @@
 
 int main(void)
 {
+    int rc = fork();
     // Your code here
-
-    return 0;
+    if(rc < 0) {    // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {    // child process satisfies this branch
+        printf("hello, child here (pid: %d) \n", (int) getpid());
+    } else {
+        int wc = waitpid(rc, NULL, 0);    // `waitpid` call added here
+        printf("hello, parent here (pid: %d) of child %d\n", (int) getpid(), rc);
+    }
 }
