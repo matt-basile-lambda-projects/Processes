005a9ef0ffdb077bb2d30f90490a2ae7ad0911f7
Matt Basile <matthewbasile2@gmail.com>
2019-05-30 14:48:36 -0400

    my ex4

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -10,6 +10,20 @@
 
 int main(void)
 {
+    int rc = fork();
+    // Your code here
+    if(rc < 0) {    // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {    // child process satisfies this branch
+        // printf("hello, child here (pid: %d) \n", (int) getpid());
+        // execl("/bin/ls", NULL);
+        execle("/bin/ls", NULL);
+        // execv("/bin/ls", NULL);
+    } else {
+        int wc = waitpid(rc, NULL, 0);    // `waitpid` call added here
+        printf("hello, parent here (pid: %d) of child %d\n", (int) getpid(), rc);
+    }
     // Your code here    
 
     return 0;
